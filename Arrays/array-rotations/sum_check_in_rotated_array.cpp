a8178b48f6de59c96ce1f462d43887a374652eb3
shivanshu1086 <shivanshu.vehiclent@gmail.com>
2020-05-01 15:25:50 +0530

    array-rotations

--- Arrays/array-rotations/sum_check_in_rotated_array.cpp
+++ Arrays/array-rotations/sum_check_in_rotated_array.cpp
@@ -2,25 +2,31 @@
 #include<string>
 using namespace std;
 
-bool pairInSortedRotated(int *a, int n, int sum){
+int pairInSortedRotated(int *a, int n, int sum){
 	int i;
 	for(i=0;i<n-1;i++)
 		if(a[i]>a[i+1])
 			break;
 	int l=(i+1)%n;//l is smallest element
 	int r=i;//r is now largest element
+	int cnt=0;
 	while(l!=r){
 		if(a[l]+a[r]==sum){
-			return true;
+			cnt++;
+			if(l==(r+n-1)%n){
+				return cnt;
+			}
+			l=(l+1)%n;
+			r=(n+r-1)%n;
 		}
-		if(a[l]+a[r]<sum){
+		else if(a[l]+a[r]<sum){
 			l=(l+1)%n;//l ko ek aage badhaya i.e. second smallest
 		}
 		else{
 			r=(n+r-1)%n;//r ko ek aage badhaya i.e second largest
 		}
 	}
-	return false;
+	return -1;
 }
 
 
@@ -28,11 +34,15 @@ int main()
 { 
     int n;
     cin>>n;
-    int a[n]; 
+    int a[n];
+	for(int i=0;i<n;i++)
+		cin>>a[i]; 
     int sum;
-    cin>>sum; 
-    if (pairInSortedRotated(a, n, sum)) 
-        cout << "Found"; 
+    cin>>sum;
+	int ans=pairInSortedRotated(a, n, sum);
+	// cout<<ans;
+    if (ans>0) 
+        cout<<ans<< " Found"; 
     else
         cout << "Not found"; 
 	cout<<endl;
